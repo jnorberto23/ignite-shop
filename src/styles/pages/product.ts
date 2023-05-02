bbd29ed58c975320c755b7a12a0a79353bff4750
jnorberto23 <joaovictor.norberto@outlook.com>
2023-05-02 16:58:23 -0300

    🚧wip on checkout api

--- src/styles/pages/product.ts
+++ src/styles/pages/product.ts
@@ -63,6 +63,14 @@ export const ProductDetails = styled('div', {
 
     '&:hover': {
       backgroundColor: '$green300',
+    },
+    '&:disabled': {
+      opacity: 0.6,
+      cursor: 'not-allowed',
+    },
+
+    '&:not(:disabled):hover': {
+      backgroundColor: '$green300',
     }
   },
 })
