50f8bf049ad1ee7b2b08ca6b53a19ebf792c98ee
jnorberto23 <joaovictor.norberto@outlook.com>
2023-05-03 10:16:58 -0300

    🚧 wip on success page

---
+++ src/pages/success.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
+export default function Success() {
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>João Norberto</strong>, sua <strong>Camiseta</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
+}
